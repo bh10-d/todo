87577163f349563ef1b0bba4c1d12d8125ebdc37
bh-10d <duchieubui25012002@gmail.com>
2021-08-21 15:45:25 -0400

    seacond commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-function addwork(){
-    var input = document.querySelector("input").value;
-    console.log(input);
-}
-document.getElementById("submit").addEventListener("click",addwork);
+let input = document.querySelector("#work");
+let btnadd = document.querySelector("#submit");
+let list = document.querySelector(".todo_list");
+// let ellist = document.querySelector(".todo_list li");
+
+
+btnadd.addEventListener("click",()=>{
+    let txt = input.value;
+    let li = document.createElement("li");
+    let span = document.createElement("span");
+    span = '<span><i class="fas fa-trash"></i></span>';
+    li.innerHTML = `<li>${txt}${span}</li>`;
+    list.insertBefore(li,list.childNodes[0]);
+    input.value = '';
+    li.addEventListener("click",()=>{
+        list.removeChild(li);
+    })
+})
