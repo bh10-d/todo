375b21dbb4969884f64042cb4f56e5f3dea05b6b
bh-10d <duchieubui25012002@gmail.com>
2021-08-26 21:19:52 -0400

    final

--- js/script.js
+++ js/script.js
@@ -7,18 +7,17 @@ let tasks = document.getElementById("count");
 let update = document.getElementsByClassName("update");
 
 var arr = [];
-var i = 0;
+var id = 0;
 
 btnadd.addEventListener("click",()=>{
     if(input.value!==""){
         let txt ={
-            id: i,//Math.floor(Math.random()*100)
+            id: id,//Math.floor(Math.random()*100)
             value: input.value
         }
         arr.push(txt);
         show();
-        i++;
-        console.log("co phan tu rong")
+        id++;
     }
 })
 
@@ -26,49 +25,39 @@ function show(){
     let textli = '';
     for (const index of arr) {
         if(index.value!==''){
-            textli+=`<li class="todo" onclick="updatetask(${index.id});">${index.value}<span onclick="deletask(${index.id});"><i class="fas fa-trash"></i></span></li>`;
+            textli+=`<li class="todo" onclick="updatetask(${index.id});">${index.value}<span onclick="deletask(${index.id});"><i class="fas fa-trash"></i></span></li> `;
         }
     }
     list.innerHTML = textli;
     input.value = '';
-    console.log(arr)
+    console.log(arr);
     alltasks();
 }
 
 function updatetask(id){
-    for (let i = 0; i < arr.length; i++) {
-        if(arr[i].id === id){
+    input.placeholder = 'Update Work';
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i].id === id){
             input.value = arr[i].value;
             btnadd.style.display = 'none';
             btnup.style.display = 'block';
             btnup.addEventListener('click',()=>{
-                arr[i].value = input.value;
-                console.log(input.value);
-                input.value = '';
-                show();
-                btnup.style.display = 'none';
-                btnadd.style.display = 'block';
+                if(i!==''){
+                    arr[i].value = input.value;
+                    console.log(input.value);
+                    console.log('i: '+i);
+                    console.log(arr[i].id + ' : ' + arr[i].value);
+                    btnup.style.display = 'none';
+                    btnadd.style.display = 'block';
+                    input.placeholder = 'Enter work';
+                    show();
+                    i='';
+                }
             })
+            // console.log(i);
         }
     }
-    console.log(id);
-    return arr;
 }
-// function up(id) {
-//     let getupdate = '';
-//     getupdate = update.value;
-//     for(let i=0; i<arr.length; i++) {
-//         if(arr[i].id === id) {
-//             arr[i].value = getupdate;
-//         }
-//     }
-//     console.log('data update: '+getupdate);
-//     console.log('success '+id);
-//     show();
-// }
-// up.addEventListener("click",()=>{
-//     console.log('sucess')
-// })
 
 function deletask(id){
     for (let i = 0; i < arr.length; i++) {
